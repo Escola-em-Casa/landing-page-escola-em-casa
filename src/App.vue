805000b3b86fd2ff77cc134e5f1226e46ae90076
EsioFreitas <esio.gustavo@gmail.com>
2020-10-14 07:55:48 -0300

    Refactory current code

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,61 @@
 <template>
-    <v-app id="app">
-        <v-main>
-            <v-container class="fill-height pa-0">
-                <v-row justify="center" align="center">
-                    <v-col
-                        cols="12"
-                        sm="12"
-                        md="6"
-
-                        class="text-center"
-                    >
-                        <v-img class="main-logo" src="@/assets/logo-with-text.png"></v-img>
-                        <v-row no-gutters>
-                            <v-col md="6" sm="12">
-                                    <v-img
-                                        class="mt-3 google-badge"
-                                        alt='Disponível no Google Play'
-                                        src='https://play.google.com/intl/en_us/badges/static/images/badges/pt-br_badge_web_generic.png'/>
-
-                            </v-col>
-                            <v-col md="6" sm="12" class="" align-self="center">
-                                <v-img src="@/assets/app-store-badge.svg" class="ma-7 apple-badge"/>
-                            </v-col>
-                        </v-row>
-                    </v-col>
-                    <v-col
-                        cols="12"
-                        sm="12"
-                        md="6"
-                    >
-                        <v-img src="@/assets/phone-frame.png" class="phone-frame"></v-img>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-main>
-    </v-app>
+  <v-app id="app">
+    <v-main>
+      <v-container class="fill-height pa-0">
+        <v-row justify="center" align="center">
+          <v-col cols="12" sm="12" md="6" class="text-center">
+            <v-img class="main-logo" src="@/assets/logo-with-text.png"></v-img>
+            <v-row no-gutters>
+              <v-col md="6" sm="12">
+                <v-img
+                  class="mt-3 google-badge"
+                  alt="Disponível no Google Play"
+                  src="https://play.google.com/intl/en_us/badges/static/images/badges/pt-br_badge_web_generic.png"
+                />
+              </v-col>
+              <v-col md="6" sm="12" class="" align-self="center">
+                <v-img
+                  src="@/assets/app-store-badge.svg"
+                  class="ma-7 apple-badge"
+                />
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="12" sm="12" md="6">
+            <v-img src="@/assets/phone-frame.png" class="phone-frame"></v-img>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-
-  export default {
-    name: 'App',
-  }
+export default {
+  name: "App",
+};
 </script>
 
 <style>
-    #app {
-        background: #2092E5;
-        margin: 0;
-        text-align: center;
-    }
+#app {
+  background: #2092e5;
+  margin: 0;
+  text-align: center;
+}
 
-    .main-logo {
-        margin: 0 auto;
-        max-height: 50vh;
-    }
+.main-logo {
+  margin: 0 auto;
+  max-height: 50vh;
+}
 
-    .google-badge {
-        height: auto;
-        width: auto;
-        max-height: 100%;
-        max-width: 100%;
-    }
+.google-badge {
+  height: auto;
+  width: auto;
+  max-height: 100%;
+  max-width: 100%;
+}
 
-    .phone-frame {
-        height: 95vh;
-    }
+.phone-frame {
+  height: 95vh;
+}
 </style>
