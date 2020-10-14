50f09d736f67c15e3368bdb167ea8754710e470a
EsioFreitas <esio.gustavo@gmail.com>
2020-10-14 09:34:25 -0300

    Add Home page

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,20 @@
 <template>
   <v-app id="app">
     <v-main>
-      <v-container class="fill-height pa-0">
-        <v-row justify="center" align="center">
-          <v-col cols="12" sm="12" md="6" class="text-center">
-            <v-img class="main-logo" src="@/assets/logo-with-text.png"></v-img>
-            <v-row no-gutters>
-              <v-col md="6" sm="12">
-                <v-img
-                  class="mt-3 google-badge"
-                  alt="Disponível no Google Play"
-                  src="https://play.google.com/intl/en_us/badges/static/images/badges/pt-br_badge_web_generic.png"
-                />
-              </v-col>
-              <v-col md="6" sm="12" class="" align-self="center">
-                <v-img
-                  src="@/assets/app-store-badge.svg"
-                  class="ma-7 apple-badge"
-                />
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col cols="12" sm="12" md="6">
-            <v-img src="@/assets/phone-frame.png" class="phone-frame"></v-img>
-          </v-col>
-        </v-row>
+      <v-container>
+        <HomePage />
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import HomePage from "./pages/HomePage/HomePage";
 export default {
   name: "App",
+  components: {
+    HomePage,
+  },
 };
 </script>
 
---
+++ src/pages/HomePage/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="HomePage" class="bg-primary">
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style></style>
